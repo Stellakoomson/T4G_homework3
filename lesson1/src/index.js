667d698d8c6480494f2dbb066bc61db3d2e30b31
Stellakoomson <stellakoomson006@gmail.com>
2024-12-16 13:53:58 +0000

    adding all files

---
+++ lesson1/src/index.js
@@ -0,0 +1,16 @@
+//alert("hello world");
+//alert("stella");
+//let name = prompt("what is your name");
+//alert(name);
+//let fufu = prompt("how much do you want to buy");
+//alert(fufu + " it is");
+
+// variable naming conventions
+let friend =  prompt("what is your friend's name");
+alert(friend);
+let age = prompt("what is your friend's age");
+alert(age);
+let favcolor =  prompt("what is your friend's favorite color");
+alert(favcolor);
+alert("My beautiful friend's name is " + friend + ". She is " + age + " years old and her favorite color is " + favcolor);
+alert(`my beautiful friend's name is ${friend}. She is ${age}.and her favourite color is ${favcolor}`)
